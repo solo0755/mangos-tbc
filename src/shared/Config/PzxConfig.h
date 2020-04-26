d66f9c965dfabba14f4b9f09423e76dad864a266
pzx <solomon0728@163.com>
2020-04-26 12:58:29 +0800

    fix uiui

--- src/shared/Config/PzxConfig.h
+++ src/shared/Config/PzxConfig.h
@@ -16,8 +16,8 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef PZXCONFIG_H
+#define PZXCONFIG_H
 
 #include "Common.h"
 #include "Policies/Singleton.h"
