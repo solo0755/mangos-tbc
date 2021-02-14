b156211970b6c5a6189a7c7fbec95c3fbcbcbe98
solomon0728@163.com <solomon0728@163.com>
2021-02-14 16:15:14 +0800

    这个顺序

--- src/game/AI/ScriptDevAI/scripts/examples/example_creature.cpp
+++ src/game/AI/ScriptDevAI/scripts/examples/example_creature.cpp
@@ -264,30 +264,6 @@ bool GossipHello_example_creature(Player* pPlayer, Creature* pCreature)
 		pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, u8"|cFF990066|TInterface\\ICONS\\INV_Holiday_Christmas_Present_02.blp:30|t|r 领取新手大礼包~~(|cffff0000必选|r)", GOSSIP_SENDER_MAIN, 101);
 		pPlayer->ADD_GOSSIP_ITEM(7, u8"|cFF990066|TInterface\\ICONS\\INV_Misc_Gift_03.blp:30|t|r免费领取-|cff6247c8职业套装|h|r", GOSSIP_SENDER_MAIN, 400);
 	}
-
-	if (pCreature->GetEntry() == sPzxConfig.GetIntDefault("pzx.init.npc", 198601)) {
-		if (!pPlayer->IsInCombat() || pPlayer->IsGameMaster()) {//战斗中不显示菜单
-			
-			//pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, u8"传送--> 沙塔斯城（|cffFF00c8新手接待|r）", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-
-			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, u8"|cFF990066|TInterface\\ICONS\\Spell_Arcane_PortalThunderBluff.blp:30|t|r传送--> 其他区域", GOSSIP_SENDER_MAIN, 300);
-
-			//if (sPzxConfig.GetIntDefault("show.morebuff", 1)) {
-			//	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, u8"一键全BUFF、满血蓝怒、修理、冷却", GOSSIP_SENDER_MAIN, 208);
-			//}
-			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, u8"|cFF990066|TInterface\\ICONS\\INV_Misc_Gift_01.blp:30|t|r免费学习-|cff6247c8商业技能|h|r", GOSSIP_SENDER_MAIN, 301);
-
-			if (sPzxConfig.GetIntDefault("show.additem", 1)) {
-				pPlayer->ADD_GOSSIP_ITEM_EXTENDED(6, u8"|cFF990066|TInterface\\ICONS\\Mail_GMIcon.blp:30|t|r输入|cff0070dd物品ID|r获取限制物品", GOSSIP_SENDER_MAIN, 777, u8"在弹框中输入物品ID编号 数量\n 例:|cFF00F0ff需要4个无底包|r，请输入:|cFFF0FF0014156   4|r", 0, true);
-			}
-
-			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->天赋重置", GOSSIP_SENDER_MAIN, 105, u8"确定要|cff0070dd重置天赋|r吗?", 0, false);
-			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->角色更名", GOSSIP_SENDER_MAIN, 106, u8"确定要|cff0070dd更改此角色的名称|r吗?", 0, false);
-			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->清理副本CD", GOSSIP_SENDER_MAIN, 108, u8"确定要|cff0070dd清理所有副本CD|r吗?", 0, false);
-
-		}
-	}
-
 	Tokens tokensNames = StrSplit(sPzxConfig.GetStringDefault("pzx.vendor.MenuNames", ""), ",");
 	for (auto& tokenName : tokensNames)
 	{
@@ -311,22 +287,44 @@ bool GossipHello_example_creature(Player* pPlayer, Creature* pCreature)
 				}
 			}
 			if (pCreature->GetEntry() == ids[(ids.size() - 1)]) {
-				std::string tiltle(name + "_");
+				std::string tiltle(name+"_");
 				Tokens tokensTitles = StrSplit(sPzxConfig.GetStringDefault(tiltle, ""), ";");
 				std::vector<std::string> titles;
 				for (auto& tokenTi : tokensTitles)
 				{
 					titles.push_back(tokenTi.c_str());
 				}
-				if (titles.size() == ids.size() - 1) {
+				if (titles.size() == ids.size()-1) {
 
-					for (int i = 0; i<ids.size() - 1; i++)
+					for (int i = 0; i<ids.size()-1; i++)
 					{
 						pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, titles[i], GOSSIP_SENDER_MAIN, ids[i]);
 					}
 				}
 				break;
 			}
+		}
+	}
+	if (pCreature->GetEntry() == sPzxConfig.GetIntDefault("pzx.init.npc", 198601)) {
+		if (!pPlayer->IsInCombat() || pPlayer->IsGameMaster()) {//战斗中不显示菜单
+			
+			//pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, u8"传送--> 沙塔斯城（|cffFF00c8新手接待|r）", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, u8"|cFF990066|TInterface\\ICONS\\Spell_Arcane_PortalThunderBluff.blp:30|t|r传送--> 其他区域", GOSSIP_SENDER_MAIN, 300);
+
+			//if (sPzxConfig.GetIntDefault("show.morebuff", 1)) {
+			//	pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, u8"一键全BUFF、满血蓝怒、修理、冷却", GOSSIP_SENDER_MAIN, 208);
+			//}
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, u8"|cFF990066|TInterface\\ICONS\\INV_Misc_Gift_01.blp:30|t|r免费学习-|cff6247c8商业技能|h|r", GOSSIP_SENDER_MAIN, 301);
+
+			if (sPzxConfig.GetIntDefault("show.additem", 1)) {
+				pPlayer->ADD_GOSSIP_ITEM_EXTENDED(6, u8"|cFF990066|TInterface\\ICONS\\Mail_GMIcon.blp:30|t|r输入|cff0070dd物品ID|r获取限制物品", GOSSIP_SENDER_MAIN, 777, u8"在弹框中输入物品ID编号 数量\n 例:|cFF00F0ff需要4个无底包|r，请输入:|cFFF0FF0014156   4|r", 0, true);
+			}
+
+			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->天赋重置", GOSSIP_SENDER_MAIN, 105, u8"确定要|cff0070dd重置天赋|r吗?", 0, false);
+			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->角色更名", GOSSIP_SENDER_MAIN, 106, u8"确定要|cff0070dd更改此角色的名称|r吗?", 0, false);
+			pPlayer->ADD_GOSSIP_ITEM_EXTENDED(GOSSIP_ICON_CHAT, u8"->清理副本CD", GOSSIP_SENDER_MAIN, 108, u8"确定要|cff0070dd清理所有副本CD|r吗?", 0, false);
+
 		}
 	}
 	//pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, u8"切换[占星者/奥尔多]声望", GOSSIP_SENDER_MAIN, 206);
